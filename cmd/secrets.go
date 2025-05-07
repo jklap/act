e9832c59964c9002504856e828430bb3742861e0
Jason Klapste <jklapste@gmail.com>
2025-05-07 00:28:09 -0500

    allow for empty env var for VAR or SECRET

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -16,12 +16,15 @@ func newSecrets(secretList []string) secrets {
 	for _, secretPair := range secretList {
 		secretPairParts := strings.SplitN(secretPair, "=", 2)
 		secretPairParts[0] = strings.ToUpper(secretPairParts[0])
-		if strings.ToUpper(s[secretPairParts[0]]) == secretPairParts[0] {
+		_, exists := s[secretPairParts[0]]
+		if exists {
 			log.Errorf("Secret %s is already defined (secrets are case insensitive)", secretPairParts[0])
 		}
 		if len(secretPairParts) == 2 {
 			s[secretPairParts[0]] = secretPairParts[1]
-		} else if env, ok := os.LookupEnv(secretPairParts[0]); ok && env != "" {
+		} else if env, ok := os.LookupEnv(secretPairParts[0]); ok {
+			// note: we intentionally do not check to see if the value is empty as an empty value is allowed
+			// we do check to make sure it was set -- if it wasn't then we will prompt for it
 			s[secretPairParts[0]] = env
 		} else {
 			fmt.Printf("Provide value for '%s': ", secretPairParts[0])
